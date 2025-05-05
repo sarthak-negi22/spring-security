b1a5b58662c0b88860c0e6fe07d6965ea1d17b74
Sarthak <saryatnam22@gmail.com>
2025-05-05 21:03:18 +0530

    feat: add custom security filter for basic authentication

---
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration      // tells spring that this class provides config to the application context
+@EnableWebSecurity      // activates spring security. Can create custom security filter
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());        // authenticate every request
+//        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());     // using basic authentication with default settings
+        return http.build();
+    }
+}
+
+// instead of a form, the basic authentication opens an alert box, and we can't access "/logout".
+// to logout, we just have to close the session
