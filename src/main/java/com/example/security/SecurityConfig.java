e32131178de965156cd61bbeab9e256644a58c94
Sarthak <saryatnam22@gmail.com>
2025-05-09 20:11:10 +0530

    feat: add in-memory authentication

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -23,9 +27,26 @@ public class SecurityConfig {
         http.httpBasic(withDefaults());     // using basic authentication with default settings
         return http.build();
     }
+
+    @Bean
+//    creating in-memory authentication
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}password1")    // this prefix is added to tell spring, to store this password as plain text
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin123")    // this prefix is added to tell spring, to store this password as plain text
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
+    }
 }
 
 // instead of a form, the basic authentication opens an alert box, and we can't access "/logout" and "/login"
 // to logout, we just have to close the session
 // in basic authentication, session is being managed by cookies
 // in form based authentication, paylod tab contains the login credentials entered by user to authenticate
+
+//  in postman, under Request Headers named as "Authorization", the encoded value is "username:password" which we enter
