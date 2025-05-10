c240d01f41a3ba839b9941bd82a95e188798fd77
Sarthak <saryatnam22@gmail.com>
2025-05-10 13:55:04 +0530

    feat: configure database to store login credentials

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+-- for every application start, to load an sql file for initializing a schema like this, for in memory h2-database
+
+create table users(username varchar_ignorecase(50) not null primary key,password varchar_ignorecase(500) not null,enabled boolean not null);
+create table authorities (username varchar_ignorecase(50) not null,authority varchar_ignorecase(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
+create unique index ix_auth_username on authorities (username,authority);
