fc471886f892c9607ccdcffc5490fc038b715f11
Sarthak <saryatnam22@gmail.com>
2025-05-13 19:14:13 +0530

    feat: configure spring security for JWT authentication

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.example.security;
 
+import com.example.security.jwt.AuthEntryPointJwt;
+import com.example.security.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -30,17 +35,31 @@ public class SecurityConfig {
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll()     // allows unauthenticated access to h2 console
-                     .anyRequest().authenticated());        // authenticate every request
+        http.authorizeHttpRequests((authorizedRequests) -> authorizedRequests.requestMatchers("/h2-console/**").permitAll()     // allows unauthenticated access to h2 console
+                .requestMatchers("/api/signin").permitAll()     // opening the access to signin
+                .anyRequest().authenticated());        // authenticate every request
 
 //        making the http request stateless (no cookies maintained)
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());     // using basic authentication with default settings
+//        http.httpBasic(withDefaults());     // using basic authentication with default settings
+        http.exceptionHandling(exception ->
+            exception.authenticationEntryPoint(unauthorizedHandler)
+        );
         http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));        // allows h2 console to be displayed in a frame from the same origin
         http.csrf(AbstractHttpConfigurer::disable);     //disabled CSRF protection for testing h2-database
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -68,6 +87,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();     // Bcrypt algo uses "salting" for encoding, it takes a random string called "salt" and concat it with raw password, then entire string is encoded adding two layers of security
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
     }
 }
 
